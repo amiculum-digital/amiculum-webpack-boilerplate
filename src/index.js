301ea4c8d41c59c9aa9b7b4c92fb1ce896bef7fc
Lee Torson <Lee.Torson@amiculum.biz>
2019-02-18 14:50:54 +0000

    removed head merge in index.js

--- src/index.js
+++ src/index.js
@@ -3,13 +3,10 @@ require('offline-plugin/runtime').install();
 import './fonts/libre-baskerville-v5-latin-regular.woff';
 import './fonts/libre-baskerville-v5-latin-regular.woff2';
 
-<<<<<<< HEAD
 import './html/index.pug';
 import './html/about-us.pug';
 import './html/html.pug';
 
-=======
 import './index.html';
->>>>>>> 896e1b785636d76acbca6a020812ab2fe2b12a16
 import './index.scss';
 import './scripts/script.js';
