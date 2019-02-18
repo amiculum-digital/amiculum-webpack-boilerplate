42a85430638a5b3c38f73add41c0d98fe2d9a171
Lee Torson <Lee.Torson@amiculum.biz>
2019-02-18 14:48:10 +0000

    Tailwind added, however purgecss strips out classes. So you need to reload the styles if you create new tailwind classes via html

--- postcss.config.js
+++ postcss.config.js
@@ -1,14 +1,19 @@
 const purgecss = require('@fullhuman/postcss-purgecss');
+const tailwindcss = require('tailwindcss');
 
 module.exports = {
   plugins: [
+    tailwindcss('./tailwind.js'),
     require('autoprefixer'),
     require('cssnano')({
         preset: 'default',
     }),
     purgecss({
-      content: ['./**/*.html'],
-      keyframes: true
+      content: [
+        './**/*.html',
+        './**/*.pug'
+      ],
+      keyframes: true,
     })
-  ]
+  ],
 }
